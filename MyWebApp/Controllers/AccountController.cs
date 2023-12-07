247449004ad6350e0be0164e568cbbb131bca4cf
Changhui Xu <changhuixu@hotmail.com>
2023-12-07 13:16:18 -0600

    update Solution to .NET 8

--- MyWebApp/Controllers/AccountController.cs
+++ MyWebApp/Controllers/AccountController.cs
@@ -1,99 +1,89 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MyWebApp.Controllers
+namespace MyWebApp.Controllers;
+
+[Route("[controller]")]
+[ApiController, Produces("application/json")]
+public class AccountController(UserManager<IdentityUser> userManager) : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController, Produces("application/json")]
-    public class AccountController : ControllerBase
+    [HttpPost("Register")]
+    public async Task<ActionResult> Register([FromBody] UserRegistrationRequest request)
     {
-        private readonly UserManager<IdentityUser> _userManager;
-
-        public AccountController(UserManager<IdentityUser> userManager)
+        if (!ModelState.IsValid)
         {
-            _userManager = userManager;
+            return BadRequest(ModelState);
         }
 
-        [HttpPost("Register")]
-        public async Task<ActionResult> Register([FromBody] UserRegistrationRequest request)
+        var identityUser = new IdentityUser { UserName = request.UserName, Email = request.Email };
+        var result = await userManager.CreateAsync(identityUser, request.Password);
+        if (!result.Succeeded)
         {
-            if (request == null || !ModelState.IsValid)
+            foreach (var error in result.Errors)
             {
-                return BadRequest(ModelState);
+                ModelState.AddModelError(error.Code, error.Description);
             }
 
-            var identityUser = new IdentityUser { UserName = request.UserName, Email = request.Email };
-            var result = await _userManager.CreateAsync(identityUser, request.Password);
-            if (!result.Succeeded)
-            {
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(error.Code, error.Description);
-                }
-
-                return BadRequest(ModelState);
-            }
-
-            return Ok();
+            return BadRequest(ModelState);
         }
 
-        [HttpPost("Login")]
-        public async Task<IActionResult> Login([FromBody] LoginCredentials credentials)
-        {
-            if (credentials == null || !ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var identityUser = await _userManager.FindByNameAsync(credentials.UserName);
-            if (identityUser == null)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var result = _userManager.PasswordHasher.VerifyHashedPassword(identityUser, identityUser.PasswordHash, credentials.Password);
-            if (result == PasswordVerificationResult.Failed)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var claims = new List<Claim>
-            {
-                new(ClaimTypes.Email, identityUser.Email),
-                new(ClaimTypes.Name, identityUser.UserName)
-            };
-
-            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-
-            return Ok();
-        }
-
-        [HttpPost("Logout")]
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return Ok();
-        }
+        return Ok();
     }
 
-    public class UserRegistrationRequest
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] LoginCredentials credentials)
     {
-        [Required] public string UserName { get; set; }
-        [Required] public string Password { get; set; }
-        [Required] public string Email { get; set; }
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var identityUser = await userManager.FindByNameAsync(credentials.UserName);
+        if (identityUser == null)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = userManager.PasswordHasher.VerifyHashedPassword(identityUser, identityUser.PasswordHash ?? string.Empty, credentials.Password);
+        if (result == PasswordVerificationResult.Failed)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Email, identityUser.Email?? string.Empty),
+            new(ClaimTypes.Name, identityUser.UserName?? string.Empty)
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+        return Ok();
     }
 
-    public class LoginCredentials
+    [HttpPost("Logout")]
+    public async Task<IActionResult> Logout()
     {
-        [Required] public string UserName { get; set; }
-        [Required] public string Password { get; set; }
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return Ok();
     }
+}
+
+public class UserRegistrationRequest
+{
+    [Required] public string UserName { get; set; } = string.Empty;
+    [Required] public string Password { get; set; } = string.Empty;
+    [Required] public string Email { get; set; } = string.Empty;
+}
+
+public class LoginCredentials
+{
+    [Required] public string UserName { get; set; } = string.Empty;
+    [Required] public string Password { get; set; } = string.Empty;
 }
